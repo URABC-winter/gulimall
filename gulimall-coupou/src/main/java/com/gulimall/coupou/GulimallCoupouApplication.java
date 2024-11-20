6ad1e22e00ea5c27a57d45fee19e5871a0dbde2b
李俊杰 <geekli.cn@gmail.com>
2024-11-20 23:39:57 +0800

    init gulimall

---
+++ gulimall-coupou/src/main/java/com/gulimall/coupou/GulimallCoupouApplication.java
@@ -0,0 +1,13 @@
+package com.gulimall.coupou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallCoupouApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallCoupouApplication.class, args);
+    }
+
+}
