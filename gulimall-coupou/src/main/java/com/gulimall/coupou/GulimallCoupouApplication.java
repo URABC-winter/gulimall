44590ad08accecb55c2bdb6a60bd0d707e60612e
李俊杰 <geekli.cn@gmail.com>
2024-11-25 01:10:11 +0800

    成功初始运行coupon

--- gulimall-coupou/src/main/java/com/gulimall/coupou/GulimallCoupouApplication.java
+++ gulimall-coupou/src/main/java/com/gulimall/coupou/GulimallCoupouApplication.java
@@ -3,11 +3,10 @@ package com.gulimall.coupou;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient
 @SpringBootApplication
+@EnableDiscoveryClient
 @MapperScan("com.gulimall.coupou.mapper")
 public class GulimallCoupouApplication {
 
